b3d16b1a6a5f1d884bc0829a12786de94f04eae3
theKorax <zikoramadin@gmail.com>
2023-09-05 16:35:16 +0100

    Updated DestroyOutOfBounds.cs

--- Lesson2_Food/Assets/Scripts/DestroyOutOfBounds.cs
+++ Lesson2_Food/Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,11 +16,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object goes past the player's view in the game, remove that object
         if(transform.position.z > topBound)
         {
             Destroy(gameObject);
         } else if(transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
