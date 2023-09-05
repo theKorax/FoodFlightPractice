d9dcd7a969f5664350e67a3da5ce998a6dc3cec2
theKorax <zikoramadin@gmail.com>
2023-09-05 15:15:04 +0100

    Updated SpawnManager.cs
    Updated the script to allow animals in our animalPrefabs array to spawn randomly based on a random selection of their index.

--- Lesson2_Food/Assets/Scripts/SpawnManager.cs
+++ Lesson2_Food/Assets/Scripts/SpawnManager.cs
@@ -14,6 +14,10 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
