098703deca5e03e55b19ed4d349901a86f8d5f8f
theKorax <zikoramadin@gmail.com>
2023-09-05 15:24:23 +0100

    Updated SpawnManager.cs
    Updated the script to spawn the animals at random positions

--- Lesson2_Food/Assets/Scripts/SpawnManager.cs
+++ Lesson2_Food/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    float spawnRangeX = 20.0f;
+    float spawnPosZ = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,8 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
