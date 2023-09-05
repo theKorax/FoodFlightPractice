ff3bf4f3b568dea5662e629a1afb3a6faf421627
theKorax <zikoramadin@gmail.com>
2023-09-05 10:24:52 +0100

    Update PlayerController.cs

--- Lesson2_Food/Assets/Scripts/PlayerController.cs
+++ Lesson2_Food/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +16,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
